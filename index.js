4dde7db5779df25ba11f1c37145f2679d50484c6
Shelley Brook <sbrook13@gmail.com>
2020-06-29 18:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(z){
+  return document.querySelector(z)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested.target')
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .getElementByTagName('ul li');
+  for (let i=0;i<list.length;i++){
+    lis[i].innerHTML = (lis[i].innerHTML+n).toString()
+  }
+}
